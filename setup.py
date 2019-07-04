f6316367a3c69a97b2074c19aee1f1a9c813a069
Hermann Krumrey <hermann@krumreyh.com>
2019-07-04 21:44:24 +0200

    Change license to GPLv3

--- setup.py
+++ setup.py
@@ -2,6 +2,19 @@
 Copyright 2019 Hermann Krumrey <hermann@krumreyh.com>
 
 This file is part of securiphant.
+
+securiphant is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+securiphant is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with securiphant.  If not, see <http://www.gnu.org/licenses/>.
 LICENSE"""
 
 
@@ -58,7 +71,7 @@ if __name__ == "__main__":
         author="Hermann Krumrey",
         author_email="hermann@krumreyh.com",
         classifiers=[
-            "License :: Other/Proprietary License"
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
         ],
         url="https://gitlab.namibsun.net/namibsun/python/securiphant",
         license="GNU GPL3",
